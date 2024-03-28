ef9778dfb0007b96401e462780f90ce86a24ec8a
SachinDhadwal122 <sachin.dhadwal@niit.com>
2024-03-28 16:07:47 +0530

    files added

---
+++ src/main/java/com/stackroute/gitlabCIDemo/GitlabCiDemoApplication.java
@@ -0,0 +1,21 @@
+package com.stackroute.gitlabCIDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitlabCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabCiDemoApplication.class, args);
+	}
+
+	@GetMapping("users")
+	public String getUser(){
+		return "Hello All Users";
+	}
+
+}
---
+++ src/test/java/com/stackroute/gitlabCIDemo/GitlabCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.gitlabCIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabCiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
